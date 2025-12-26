864826b2ba403e08b48b8ba7a488ed3c958fcb5c
smartful <remi_rodrigues@hotmail.com>
2025-12-26 18:50:42 +0100

    Initial Commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import favicon from '$lib/assets/favicon.svg';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+<h1>Remote Blog</h1>
+
+{@render children()}
