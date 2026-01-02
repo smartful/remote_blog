c9a33951b4e9246217a6e8c1ecc02733391a97cf
smartful <remi_rodrigues@hotmail.com>
2026-01-02 15:52:51 +0100

    get All Posts

---
+++ src/routes/admin/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { get_all_posts } from './posts.remote';
+
+	// Query to get all posts
+	const posts = await get_all_posts();
+</script>
+
+<!-- List all posts -->
+<ol>
+	{#each posts as post (post.id)}
+		<li>{post.title}</li>
+	{/each}
+</ol>
