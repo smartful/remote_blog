a5937cb35c8e8b8f23e9387edf07d1f6fb05ca3c
smartful <remi_rodrigues@hotmail.com>
2026-01-02 20:09:16 +0100

    Create a Post

---
+++ src/routes/admin/post/new/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { create_post } from '../../posts.remote';
+</script>
+
+<form {...create_post}>
+	<div class="row">
+		<label>
+			Title:
+			<input {...create_post.fields.title.as('text')} />
+		</label>
+		{#each create_post.fields.title.issues() as issue (issue.message)}
+			<p>{issue.message}</p>
+		{/each}
+	</div>
+	<div class="row">
+		<label>
+			Body: <textarea {...create_post.fields.body.as('text')}></textarea>
+		</label>
+	</div>
+	<button type="submit">Post</button>
+</form>
--- src/routes/admin/posts.remote.ts
+++ src/routes/admin/posts.remote.ts
@@ -1,4 +1,34 @@
-import { query } from '$app/server';
+import { form, getRequestEvent, query } from '$app/server';
+import { error, redirect } from '@sveltejs/kit';
+import * as v from 'valibot';
 import { db } from '$lib/server/db';
+import { auth } from '$lib/auth';
+import { post } from '$lib/server/db/schema';
 
 export const get_all_posts = query(async () => db.query.post.findMany());
+
+export const create_post = form(
+	v.object({
+		title: v.pipe(v.string(), v.nonEmpty('Title is required')),
+		body: v.pipe(v.string(), v.nonEmpty())
+	}),
+	async ({ title, body }) => {
+		const event = getRequestEvent();
+		const session = await auth.api.getSession({
+			headers: event.request.headers
+		});
+
+		if (!session?.user.id) {
+			error(401, 'Unauthorized');
+		}
+
+		const slug = title.toLowerCase().replace(/ /g, '-');
+		await db.insert(post).values({
+			title,
+			slug,
+			body,
+			authorId: session.user.id
+		});
+		redirect(303, '/admin');
+	}
+);
