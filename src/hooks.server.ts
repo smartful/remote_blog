8b650fd92a8992e93382181f0130d563c9c351d1
smartful <remi_rodrigues@hotmail.com>
2026-01-02 12:01:40 +0100

    add middleware around auth

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -3,9 +3,20 @@ import type { Handle } from '@sveltejs/kit';
 import { sequence } from '@sveltejs/kit/hooks';
 import { building } from '$app/environment';
 import { auth } from '$lib/auth';
+import { resolve } from 'path';
 
-export const authHandle: Handle = async ({ event, resolve }) => {
+const authHandle: Handle = async ({ event, resolve }) => {
 	return svelteKitHandler({ event, resolve, auth, building });
 };
 
-export const handle = sequence(authHandle);
+const sessionHandle: Handle = async ({ event, resolve }) => {
+	const session = await auth.api.getSession({
+		headers: event.request.headers
+	});
+
+	event.locals.user = session?.user;
+	const response = await resolve(event);
+	return response;
+};
+
+export const handle = sequence(authHandle, sessionHandle);
